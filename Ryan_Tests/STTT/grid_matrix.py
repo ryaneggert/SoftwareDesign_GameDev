5ded55bce662967328bcc59625b6822c0f3f6d0f
Meg McCauley <megan@mccauley.net>
2014-10-21 17:37:51 -0400

    Added a matrix to keep track of the grid status

---
+++ Ryan_Tests/STTT/grid_matrix.py
@@ -0,0 +1,12 @@
+# grid_matrix.property
+
+ring_none = []
+full_board = []
+
+for num in range(8):
+	ring_none.append(None)
+print ring_none
+
+for num in range(4):
+	full_board.append(ring_none)
+print full_board
