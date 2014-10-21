8a38e87970756338a44acac3ea56bdbb0e53d308
Meg McCauley <megan@mccauley.net>
2014-10-20 21:41:56 -0400

    Added blue and purple bugs as .png files. Fixed image.py. Added place_bug.py which puts a bug at a center location

---
+++ Ryan_Tests/STTT/place_bug.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 20 20:37:28 2014
+
+@author: Meg McCauley
+
+Given the cartesian center of a sector (sectorcenter) of a gameboard, draws a semitransparent icon over that sector (for hovering)
+
+If statement: If this is player1, make it red, player2, make it blue
+
+NEED: blue bug, purple bug (for now, use different images)
+Ability to make transparent bugs (being worked on in image)
+"""
+import pygame as pyg
+from pygame.locals import *
+
+def main():
+    pyg.init()
+    screen = pyg.display.set_mode((800,800))
+    place_bug(screen,3)
+
+def drawhovericon(sectorcenter): #also eventually intake (player)
+    pass
+#    if player == 1:
+#        image = red_bug
+#    elif player == 2:
+#        image = blue_bug
+#    else player == 3:
+#        image = purple_bug
+    
+def place_bug(screen,player):
+    if player == 1:
+        image = pyg.image.load('redbug.png')
+    elif player == 2:
+        image = pyg.image.load('bluebug.png')
+    elif player == 3:
+        image = pyg.image.load('purplebug.png')
+    screen.fill((50,100,100))
+    rect = image.get_rect()
+    rect_coord = rect_x, rect_y = (rect.centerx, rect.centery)
+    coord = width,height = (50,50)
+    new_image = pyg.transform.scale(image, (coord))
+    screen.blit(new_image,(rect_coord))
+    pyg.display.flip()
+    
+main()
